1c559d01435beaf333c3b2d4d1fccc5576bf50df
Neytfliz <giorgiogrillo2012@gmail.com>
2024-11-17 11:12:01 +0100

    Update grimbo.py

--- grimbo.py
+++ grimbo.py
@@ -12,47 +12,62 @@ async def on_ready():
     print(f'Abbiamo fatto l\'accesso come {bot.user}')
 @bot.command()
 async def ciao(ctx):
-    await ctx.send("Ciao!")
+    await ctx.send("ciao")
 @bot.command()
 async def pasw(ctx):
     await ctx.send(gen_pass(10))
-bot.run("")
-    elif message.content.startswith("$fuoco")
-        await message.channel.send("fuoco è debole ad acqua, roccia e terra.")
-    elif message.content.startswith("$acqua"):
-        await message.channel.send("acqua è debole a erba ed elettro.")
-    elif message.content.startswith("$erba"):
-        await message.channel.send("erba è debole a fuoco, veleno, volante, ghiaccio e coleotero.")
-    elif message.content.startswith("$elettro"):
-        await message.channel.send("elettro è debole a terra.")
-    elif message.content.startswith("$ghiaccio"):
-        await message.channel.send("ghiaccio è debole a fuoco, lotta, roccia e acciaio.")
-    elif message.content.startswith("$lotta"):
-        await message.channel.send("lotta è debole a volante, psico e folletto.")
-    elif message.content.startswith("$veleno"):
-        await message.channel.send("veleno è debole a psico e terra.")
-    elif message.content.startswith("$terra"):
-        await message.channel.send("terra è debole ad acqua, erba e ghiaccio.")
-    elif message.content.startswith("$volante"):
-        await message.channel.send("volante è debole a elettro, ghiaccio e roccia.")
-    elif message.content.startswith("$psico"):
-        await message.channel.send("psico è debole a coleottero, buio e spettro.")
-    elif message.content.startswith("$coleottero"):
-        await message.channel.send("coleottero è debole a fuoco, roccia e volante.")
-    elif message.content.startswith("$roccia"):
-        await message.channel.send("roccia è debole a lotta, acciaio, erba, acqua e terra.")
-    elif message.content.startswith("$spettro"):
-        await message.channel.send("spettro è debole a spettro e buio.")
-    elif message.content.startswith("$buio"):
-        await message.channel.send("buio è debole a lotta, folletto e coleottero.")
-    elif message.content.startswith("$drago"):
-        await message.channel.send("drago è debole a drago, ghiaccio e folletto.")
-    elif message.content.startswith("$acciaio"):
-        await message.channel.send("acciaio è debole a lotta, fuoco e terra.")
-    elif message.content.startswith("$folletto"):
-        await message.channel.send("folletto è debole a veleno e acciaio.")
-    elif message.content.startswith("$normale"):
-        await message.channel.send("normale è debole a terra.")
-
-
-client.run("insert_token")
+@bot.command()
+async def fire(ctx):
+    await ctx.send("fuoco è debole ad acqua, roccia e terra.")
+@bot.command()
+async def watr(ctx):
+    await ctx.send("acqua è debole a erba ed elettro.")
+@bot.command()
+async def gras(ctx):
+    await ctx.send("erba è debole a fuoco, veleno, volante, ghiaccio e coleottero.")
+@bot.command()
+async def elct(ctx):
+    await ctx.send("elettro è debole a terra.")
+@bot.command()
+async def ice(ctx):
+    await ctx.send("ghiaccio è debole a fuoco, lotta, roccia e acciaio.")
+@bot.command()
+async def fght(ctx):
+    await ctx.send("lotta è debole a volante, psico e folletto.")
+@bot.command()
+async def posn(ctx):
+    await ctx.send("veleno è debole a terra e psico.")
+@bot.command()
+async def grnd(ctx):
+    await ctx.send("terra è debole a erba, acqua e ghiaccio.")
+@bot.command()
+async def flng(ctx):
+    await ctx.send("volante è debole a roccia, ghiaccio ed elettro.")
+@bot.command()
+async def psyc(ctx):
+    await ctx.send("psico è debole a coleottero, buio e spettro.")
+@bot.command()
+async def bug(ctx):
+    await ctx.send("coleottero è debole a fuoco, roccia e volante.")
+@bot.command()
+async def rock(ctx):
+    await ctx.send("roccia è debole a lotta, acciaio, erba, acqua e terra.")
+@bot.command()
+async def ghst(ctx):
+    await ctx.send("spettro è debole a spettro e buio.")
+@bot.command()
+async def dark(ctx):
+    await ctx.send("buio è debole a lotta, folletto e coleottero.")
+@bot.command()
+async def drgn(ctx):
+    await ctx.send("drago è debole a drago, ghiaccio e folletto.")
+@bot.command()
+async def stel(ctx):
+    await ctx.send("acciaio è debole a lotta, fuoco e terra.")
+@bot.command()
+async def fary(ctx):
+    await ctx.send("folletto è debole a veleno e acciaio.")
+@bot.command()
+async def nrml(ctx):
+    await ctx.send("normale è debole a lotta.")
+bot.run("insert_token")
